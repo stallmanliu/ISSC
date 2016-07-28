9dfd1d1e197d3317d585e69cec690f12542ab5f0
root <root@example.com>
2016-07-28 15:51:49 -0400

    remove vendor/bundle

--- vendor/bundle/ruby/1.9.1/gems/passenger-4.0.60/packaging/debian/internal/test/misc/nodejs_test_app.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end("Hello Node.js\n");
-});
-server.listen('127.0.0.1', 3000);
